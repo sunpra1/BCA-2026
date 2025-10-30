463f8ffbe6387a1fbce931bc96337650a047cd68
SUNPRA <sunpra@gmail.com>
2025-10-30 08:01:07 +0545

    Oct 30, 2025

--- app/src/main/java/com/sunpra/bca_2026/InputFieldActivity.java
+++ app/src/main/java/com/sunpra/bca_2026/InputFieldActivity.java
@@ -3,14 +3,17 @@ package com.sunpra.bca_2026;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.graphics.Insets;
@@ -29,6 +32,7 @@ public class InputFieldActivity extends AppCompatActivity {
     CheckBox economicsCb;
     CheckBox computerCb;
     SwitchCompat isDifferentlyAbledSwitch;
+    TextView messageTv;
 
     Button submitBtn;
 
@@ -57,6 +61,7 @@ public class InputFieldActivity extends AppCompatActivity {
         computerCb = findViewById(R.id.computerCb);
         submitBtn = findViewById(R.id.submitBtn);
         isDifferentlyAbledSwitch = findViewById(R.id.isDifferentlyAbledSwitch);
+        messageTv = findViewById(R.id.messageTv);
 
         submitBtn.setOnClickListener(
                 (View view) -> {
@@ -66,7 +71,53 @@ public class InputFieldActivity extends AppCompatActivity {
         );
 
 
+        gradeSpinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(
+                            AdapterView<?> adapterView,
+                            View view,
+                            int selectedPosition,
+                            long id
+                    ) {
+                        String[] grades = getResources().getStringArray(R.array.grade_items);
+                        String selectedGrade = grades[selectedPosition];
+                        messageTv.setText("You have selected grade: " + selectedGrade);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        messageTv.setText("Grade not selected.");
+                    }
+                }
+        );
+
+        genderGroup.setOnCheckedChangeListener(
+                (@NonNull RadioGroup radioGroup, int id) -> {
+                    if (id == R.id.maleRb) {
+                        messageTv.setText("You have selected male gender.");
+                    } else if (id == R.id.femaleRb) {
+                        messageTv.setText("You have selected female gender.");
+                    } else if (id == R.id.othersRb) {
+                        messageTv.setText("You have selected other gender.");
+                    }
+                }
+        );
+
+        accountCb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton compoundButton, boolean isChecked) {
+                messageTv.setText("You have selected subject, accounts.");
+            }
+        });
+
+        isDifferentlyAbledSwitch.setOnCheckedChangeListener(
+                (@NonNull CompoundButton compoundButton, boolean isChecked) -> {
+                    messageTv.setText("You have " + (isChecked ? "Checked" : "Unchecked") + "is differently able.");
+                }
+        );
+
+//        firstNameEt.addTextChangedListener();
+
     }
-
-
 }
